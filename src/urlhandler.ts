2b66229abfe2afc9e98705bfc02dd58f722d7c48
parvk11 <kparv01@gmail.com>
2024-09-02 17:01:39 -0400

    update run and urlhandler files

---
+++ src/urlhandler.ts
@@ -0,0 +1,15 @@
+export class urlhandler{
+    private url: URL;
+
+    constructor(url: string){
+        try{
+        this.url = new URL(url);
+        } catch(error){
+            throw new Error('Invalid URL');
+        }
+    }
+
+    identifyURL():{
+        
+    }
+}
