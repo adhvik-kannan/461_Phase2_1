69ef8412b31466c145b35e51f3cc1156b242bb92
parvk11 <kparv01@gmail.com>
2024-09-06 10:52:51 -0400

    api changes

--- src/urlhandler.ts
+++ src/urlhandler.ts
@@ -16,35 +16,30 @@ export class URLHandler {
         }
     }
 
-    private isValidUrl(): boolean {
-        return this.GITHUB_URL_PATTERN.test(this.url) || this.NPM_URL_PATTERN.test(this.url);
-    }
-
-    public async handle() {
-        if (this.GITHUB_URL_PATTERN.test(this.url)) {
-            // Delegate to GitAPIHandler
-            const match = this.GITHUB_URL_PATTERN.exec(this.url);
-            const owner = match ? match[1] : null;
-            const repo = match ? match[2] : null;
-
-            if (owner && repo) {
-                //const gitHandler = new GitAPIHandler(owner, repo);
-                //await gitHandler.processRepo();
-            } else {
-                console.error('Invalid GitHub URL format.');
-            }
-        } else if (this.NPM_URL_PATTERN.test(this.url)) {
-            // Delegate to npmHandler
-            const match = this.NPM_URL_PATTERN.exec(this.url);
-            const packageName = match ? match[1] : null;
-
-            if (packageName) {
-                await npmHandler.processPackage(packageName);
-            } else {
-                console.error('Invalid NPM URL format.');
-            }
-        } else {
-            console.error('Unsupported URL type.');
+    public identify(url_pattern: URL): string{
+        if(this.GITHUB_URL_PATTERN.test(url_pattern)){
+            return "GitHub";
         }
+        else if(this.NPM_URL_PATTERN.test(url_pattern)){
+            return "NPM";
+        }
+        return "Not Found";
+
     }
+
+    // handleGit(url_pattern: string){
+    //     const author = "x";
+    //     const repo = "x";
+    //     this.getCommitHistory = (owner, repo)
+
+    // }
+
+    handleNPM(url_pattern: string){
+
+    }
+
+
+
+    
+
 }
